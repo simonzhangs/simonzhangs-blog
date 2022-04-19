7870fa2dd0febc3ce2a99793f4e096e8d2926179
simonzhangs <2863389578@qq.com>
2022-04-19 14:28:55 +0800

    update:articles

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://simonzhangs.github.io&token=o9roFQBF2efJgalK"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.songbenblog.com&token=o9roFQBF2efJgalK"
 
 rm -rf urls.txt # 删除文件
