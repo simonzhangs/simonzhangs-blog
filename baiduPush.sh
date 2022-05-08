7cd80f3afb28e173934db611bcd81d5c4ef9591a
simonzhangs <2863389578@qq.com>
2022-05-08 20:13:23 +0800

    update：baiduPush

--- baiduPush.sh
+++ baiduPush.sh
@@ -3,7 +3,10 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.songbenblog.com&token=o9roFQBF2efJgalK"
+# 百度链接推送（songben）
+# curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.songbenblog.com&token=o9roFQBF2efJgalK"
+
+# 百度链接推送 （github pages）
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://simonzhangs.github.io&token=o9roFQBF2efJgalK"
 
 rm -rf urls.txt # 删除文件
